c8d7100a0b49ee029d37c0682df379088622d25a
lrs121 <lrs121@ic.ac.uk>
2024-10-19 20:33:24 +0100

    Add trivial and infeasible clauses, also add many lines (maybe not working atm)

--- generate.py
+++ generate.py
@@ -26,7 +26,7 @@ def duplicate_line(input_content, seed = None):
     lines.insert(random.randint(0, len(lines)-1), line)
     return '\n'.join(lines)
 
-def delete_random_word(input_content, seed = None):
+def delete_random_number(input_content, seed = None):
     if len(input_content) == 1:
         return input_content
     random.seed(seed)
@@ -46,6 +46,23 @@ def randomize_lines(input_content, seed = None):
     random.shuffle(lines)
     return '\n'.join(lines)
 
+def add_trivial_clause(input_content, seed = None):
+    random.seed(seed)
+    lines = input_content.split('\n')
+    lines.insert(random.randint(0, len(lines)-1), "1 1 0")
+    return '\n'.join(lines)
+
+def add_infeasible_clause(input_content, seed = None):
+    random.seed(seed)
+    lines = input_content.split('\n')
+    lines.insert(random.randint(0, len(lines)-1), "1 1 0\n-1 -1 0")
+    return '\n'.join(lines)
+
+def duplicate_many_lines(input_content, seed = None):
+    random.seed(seed)
+    [one, lines] = input_content.split('\n', 1)
+    return one + lines * 2
+
 def mutate(input_file, seed = None):
     with open(input_file, "r") as f:
         input_content = f.read()
@@ -54,7 +71,11 @@ def mutate(input_file, seed = None):
     iterations = random.randint(1, 100)
     output = input_content
     for i in range(iterations):
-        output = random.choice([delete_random_word, duplicate_line, delete_random_character, randomize_lines])(output, seed)
+        output = random.choices(
+                        [delete_random_number, duplicate_line, delete_random_character, randomize_lines, add_trivial_clause, add_infeasible_clause], 
+                        weights = [0.05, 0.05, 0.05, 0.1, 0.05, 0.7],
+                        k = 1
+                        )[0](output, seed)
 
     with open(input_file, "w") as f:
         f.write(output)
