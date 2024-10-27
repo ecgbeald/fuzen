363181e3b4ee1164230a8b34a0445ddc4737c5f7
Rickie <rm521@ic.ac.uk>
2024-10-27 16:35:10 +0000

    add stack-overflow

--- parser.py
+++ parser.py
@@ -42,6 +42,7 @@ class ErrorType(Enum):
     HEAP_USE_AFTER_FREE = auto()
     HEAP_BUFFER_OVERFLOW = auto()
 
+    STACK_OVERFLOW = auto()
     STACK_BUFFER_OVERFLOW = auto()
     STACK_BUFFER_UNDERFLOW = auto()
     STACK_USE_AFTER_SCOPE = auto()
@@ -79,6 +80,8 @@ def categorise_error(error_message: str):
     if re.search(r"AddressSanitizer: heap-buffer-overflow", error_message):
         errors.add(ErrorType.HEAP_BUFFER_OVERFLOW)
 
+    if re.search(r"AddressSanitizer: stack-overflow", error_message):
+        errors.add(ErrorType.STACK_OVERFLOW)
     if re.search(r"AddressSanitizer: stack-buffer-overflow", error_message):
         errors.add(ErrorType.STACK_BUFFER_OVERFLOW)
     if re.search(r"AddressSanitizer: stack-buffer-underflow", error_message):
@@ -110,6 +113,8 @@ def match_single_asan_error(error_message: str):
     if re.search(r"AddressSanitizer: heap-buffer-overflow", error_message):
         return ErrorType.HEAP_BUFFER_OVERFLOW
 
+    if re.search(r"AddressSanitizer: stack-overflow", error_message):
+        errors.add(ErrorType.STACK_OVERFLOW)
     if re.search(r"AddressSanitizer: stack-buffer-overflow", error_message):
         return ErrorType.STACK_BUFFER_OVERFLOW
     if re.search(r"AddressSanitizer: stack-buffer-underflow", error_message):
